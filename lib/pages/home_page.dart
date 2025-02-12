2e2d69e67113e171df4d4cb809a148a408aaafb0
T0a5t-E <gerardogongora.personal@gmail.com>
2025-02-12 09:01:57 -0600

    Main Text Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        // Left Icon
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[800]
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )],
+      ),
+      body: Column(
+        children: [
+          // Texto "I want to eat"
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(fontSize: 32)),
+                Text(
+                  "Eat", 
+                  style: TextStyle(
+                  fontSize: 32, // Font size
+                  fontWeight: FontWeight.bold, // Bold Text
+                  decoration: TextDecoration.underline // Underline
+                ))
+              ],
+            ),
+          )
+          // Tab bar
+
+          // Tab bar view
+
+          // Carrito
+
+        ],
+      ),
+    );
   }
 }
