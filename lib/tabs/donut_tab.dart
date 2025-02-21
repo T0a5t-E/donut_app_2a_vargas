b6612c2bf0d2e3bec3321be5f7eddefe2c935e17
T0a5t-E <gerardogongora.personal@gmail.com>
2025-02-21 08:46:24 -0600

    Starting GridView

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,12 +1,35 @@
+import 'package:donut_app_2a_vargas/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-  const DonutTab({super.key});
+  
+  // Donut List
+  final List donutsOnSale = [
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.red, "lib/images/grape_donut.png"],
+    ["Chocolate", "45", Colors.red, "lib/images/chocolate_donut.png"],
+  ];
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Donut Tab"),
+    return GridView.builder(
+      // Amount of elements
+      itemCount: donutsOnSale.length,
+      padding: const EdgeInsets.all(12),
+      // Organizes the grid
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        // Number of columns
+        crossAxisCount: 2),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
+      }
     );
   }
 }
